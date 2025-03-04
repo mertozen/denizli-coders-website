4f9af62e8ea86ec3bf93d338cb89bf01f161fed8
meltemozen <meltem.ozen.1999@gmail.com>
2025-03-04 16:34:56 +0300

    menu and navbar updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 import Menu from "./components/Menu";
 import Events from "./components/Events";
 import Footer from "./components/Footer";
 import Card from "./components/Card";
-
+import Rooster1 from "./assets/rooster_codes.webp";
+import denizli_coders_logo from "./assets/denizli_coders_logo.webp";
 
 function App() {
   return (
     <>
-      <Menu title={"Menu"} />
-      <Card title={"Card"} />
-      <Events title={"Events"} />
-      <Footer title={"Footer"} />
+      <Navbar logo={denizli_coders_logo} title1={"About Us"} title2= {"Events"} title3 ={"Our Team"} button={"Join Us"} />
+      <Menu image= {Rooster1} description1={"Denizlinin kod tutkunları burada buluşuyor!"} description2 = {"Kodla, geliş, fark yarat!"} button1= {"Learn More"} />
+      <Card title={""} />
+      <Events title={""} />
+      <Footer title={""} />
     </>
   );
 }
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,8 +1,42 @@
+//import rooster_codes from './assests/rooster_codes.webp'
 interface MenuProps {
-  title: string;
+  image: any;
+  description1: string;
+  description2: string;
+  button1: string;
 }
-const Menu: React.FC<MenuProps> = ({ title }) => {
-  return <div><h1 className="text-3xl font-bold underline">{title}</h1></div>;
+const Menu: React.FC<MenuProps> = ({image, description1, description2, button1}) => {
+  return (
+
+<section className="self-end mt-24 w-full max-w-[1316px] max-md:mt-10 max-md:mr-2.5 max-md:max-w-full">
+          <div className="flex items-center gap-5 max-md:flex-col">
+            <article className="w-[69%] max-md:ml-0 max-md:w-full">
+              <div className="self-stretch my-auto tracking-wider max-md:mt-10 max-md:max-w-full">
+                <div className="flex flex-col pl-4 font-medium max-md:max-w-full">
+                  <h1 className="text-5xl leading-[66px] text-slate-900 max-md:max-w-full max-md:text-4xl max-md:leading-[59px]">
+                    {description1}
+                  </h1>
+                  <h2 className="ext-5xl leading-[66px] text-slate-900 max-md:max-w-full max-md:text-4xl max-md:leading-[59px]">
+                    {description2}
+                  </h2>
+                </div>
+                <button className="overflow-hidden px-14 mt-14 max-w-full text-xl font-bold bg-blue-700 rounded-3xl h-[59px] leading-[60px] text-zinc-100 w-[241px] max-md:px-5 max-md:mt-10">
+                   {button1}
+                </button>
+              </div>
+            </article>
+            
+            <div className="mr-5 w-[60%] max-md:mt-0 max-md:w-full">
+              <img
+                src={image}
+                alt="Hero illustration"
+                className="object-contain w-full aspect-square max-md:mt-10 max-md:max-w-full"
+              />
+            </div>
+          </div>
+        </section>
+
+  )
 };
 
 export default Menu;
