511155f966e75ee8da45a1dbd70db7c3ab068e2a
meltemozen <meltem.ozen.1999@gmail.com>
2025-03-05 23:52:04 +0300

    variable names and  some components edited

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,30 +1,30 @@
 interface NavbarProps {
     logo: any;
-    title1: string;
-    title2: string;
-    title3: string;
-    button: string;
-
-    url: string;
+    about_tag: string;
+    events_tag: string;
+    team_tag: string;
+    join_button: string;
+    tg_url: string;
 
   }
-  const Navbar: React.FC<NavbarProps> = ({logo, title1,title2, title3, button, url}) => {
+  const Navbar: React.FC<NavbarProps> = ({logo, about_tag,events_tag, team_tag, join_button, tg_url}) => {
    
     return(
   
       <nav className="relative flex flex-col w-full bg-white">
-      <div className="flex items-center justify-between px-4 py-3 md:px-6">
+      <div className="flex items-center justify-between px-5 py-3 md:px+5">
+      <div className="flex items-center">
+      <a href=""><img src={logo} alt="Logo" className="object-contain w-[350px] sm:w-[200px] aspect-square" /> </a>
 
-      <a href=""><img src={logo} alt="Logo" className="object-contain w-[100px] aspect-square" /> </a>
-
-      <div className="flex flex-col md:flex-row items-center gap-x-30 text-lg font-bold text-blue-700">
-
-        <a href="#about" className="hover:text-blue-500 transition">{title1}</a>
-        <a href="#events" className="hover:text-blue-500 transition">{title2}</a>
-        <a href="#team" className="hover:text-blue-500 transition">{title3}</a>
+      <div className="flex flex-col md:flex-row items-center gap-x-6 gap-y-3 md:gap-y-0 text-lg font-bold text-blue-700">
+        <a href="#about" className="hover:text-blue-500 transition">{about_tag}</a>
+        <a href="#events" className="hover:text-blue-500 transition">{events_tag}</a>
+        <a href="#team" className="hover:text-blue-500 transition">{team_tag}</a>
       </div>
-
-      <a href={url}><button className="px-6 py-2 text-lg font-medium leading-7 text-white bg-blue-700 rounded-3xl hover:bg-blue-800 transition">{button}</button> </a>
+      </div>
+      <div className="flex sm:justify-end justify-center w-full sm:w-auto mt-2 sm:mt-0 mr-3 sm:mr-30">
+      <a href={tg_url}><button className="px-6 py-2 text-lg font-medium leading-7 text-white bg-blue-700 rounded-3xl hover:bg-blue-800 transition">{join_button}</button> </a>
+      </div>
       </div>
       </nav>
     )
